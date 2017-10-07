2e9170bd27fed5e16ce2631c30dae4d721e7a784
jerrysjam <544752173@qq.com>
2017-10-08 00:12:17 +0800

    [通用]增加问题model

---
+++ app/Question.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    //
+
+    public function isHidden()
+    {
+        return $this->is_hidden;
+    }
+}
